c02f646deca835f76abbdf1f0df3bbb14fab8999
israrahmedjan <israrahmedjan@gmail.com>
2024-06-21 08:38:57 +0500

    New messages added

--- index.js
+++ index.js
@@ -101,12 +101,12 @@ app.get("/myuploadfile", (req, res) => {
 
 app.get("/", (req, res) => {
 
-  res.send("Hello Home page!");
-  // if (!req.isAuthenticated()) {
-  //   return res.redirect('/user/login');
-  // }
+  // res.send("Hello Home page!");
+  if (!req.isAuthenticated()) {
+    return res.redirect('/user/login');
+  }
 
-  // res.render('HomeView');
+  res.render('HomeView');
 
 
 })
